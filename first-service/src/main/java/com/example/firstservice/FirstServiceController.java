46603a190d705a5ca3cd1dc5478c3d12fc4e7647
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-01-16 18:49:15 +0900

    Load Balancer

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,22 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+
+    @Autowired
+    Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +30,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port = {}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on PORT %s", env.getProperty("local.server.port"));
     }
 }
