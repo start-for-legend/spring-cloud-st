bc537c2f8397338a745e4346e406fddc3bf1ab66
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-01-28 23:46:12 +0900

    catalog-service -> kafka message topic

---
+++ catalog-service/src/main/java/com/example/catalogservice/messagequeue/KafkaConsumer.java
@@ -0,0 +1,46 @@
+package com.example.catalogservice.messagequeue;
+
+import com.example.catalogservice.entity.Catalog;
+import com.example.catalogservice.entity.CatalogRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaConsumer {
+
+    private final CatalogRepository catalogRepository;
+
+    @KafkaListener(topics = "example-catalog-topic")
+    public void updateQty(String kafkaMessage) {
+        log.info("Kafka Message: -> " + kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        Catalog catalog = catalogRepository.findByProductId((String)map.get("productID"));
+        if (catalog != null) {
+            catalog.setStock(catalog.getStock() - (Integer) map.get("qty"));
+        } else {
+            log.error("Catalog Is Null");
+        }
+        
+        catalogRepository.save(Objects.requireNonNull(catalog));
+    }
+}
